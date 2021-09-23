673ea0e7204d3901941360b236a2a8697eea5376
josedejesusAmaya <hello.devpp@gmail.com>
2021-09-23 15:53:46 -0500

    icons

--- scripts/script.js
+++ scripts/script.js
@@ -12,4 +12,19 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    $('#menu_option1').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    
+    $('#menu_option2').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    $('#menu_option3').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 });
